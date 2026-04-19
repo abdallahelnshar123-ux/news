96c7f1a60e99410277addda21627ea4cc8199ce8
abdallah elnshar <abdallah.elnshar.123@gmail.com>
2026-04-19 22:28:43 +0200

    ..

--- lib/home_screen/category_details/news/news_widget.dart
+++ lib/home_screen/category_details/news/news_widget.dart
@@ -2,12 +2,10 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:news/api/api_manager.dart';
 import 'package:news/home_screen/category_details/news/bottom_sheet_news_widget.dart';
-import 'package:news/home_screen/category_details/news/page_navigation_widget.dart';
 import 'package:news/model/news_response.dart';
 import 'package:news/model/source_response.dart';
 import 'package:news/utils/screen_size.dart';
 
-import '../../../global_widgets/main_error_widget.dart';
 import '../../../global_widgets/main_loading_widget.dart';
 
 import '../../../utils/app_Colors.dart';
@@ -23,102 +21,73 @@ class NewsWidget extends StatefulWidget {
 }
 
 class _NewsWidgetState extends State<NewsWidget> {
+  final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
+
   int pageNum = 1;
+  List<News> newsList = [];
+
+  @override
+  void initState() {
+    scrollController.addListener(onScroll);
+    super.initState();
+    loadNews();
+  }
+
+  @override
+  void didUpdateWidget(covariant NewsWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.source.id != widget.source.id) {
+      pageNum = 1;
+      newsList.clear();
+      loadNews();
+    }
+  }
+
+  @override
+  void dispose() {
+    scrollController.removeListener(onScroll);
+    scrollController.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<NewsResponse>(
-      future: ApiManager.getNewsBySourceId(widget.source.id ?? '', pageNum),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return MainLoadingWidget();
-        }
-        if (snapshot.hasError) {
-          // todo :
-          return MainErrorWidget(
-            onPressed: () {
-              ApiManager.getNewsBySourceId(widget.source.id ?? '', pageNum);
-              setState(() {});
-            },
-            errorMessage: 'Something went Wrong ',
+    if (newsList.isEmpty && isLoading) {
+      return MainLoadingWidget();
+    }
+
+    if (newsList.isEmpty) {
+      return Expanded(
+        child: Center(
+          child: Text(
+            context.tr('no_news_from_this_source'),
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+        ),
+      );
+    }
+
+    return Expanded(
+      child: ListView.separated(
+        controller: scrollController,
+        padding: EdgeInsets.all(context.width * 0.03),
+        separatorBuilder: (context, index) =>
+            SizedBox(height: context.width * 0.04),
+        itemCount: newsList.length + (isLoading ? 1 : 0),
+        itemBuilder: (context, index) {
+          if (index == newsList.length) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          return GestureDetector(
+            onTap: () => onNewsClick(newsList[index]),
+            child: NewsItem(news: newsList[index]),
           );
-        }
-
-        if (snapshot.data?.status != 'ok') {
-          return MainErrorWidget(
-            onPressed: () {
-              ApiManager.getNewsBySourceId(widget.source.id ?? '', pageNum);
-              setState(() {});
-            },
-            errorMessage: snapshot.data!.message!,
-          );
-        }
-
-        var newsList = snapshot.data!.articles!;
-        return newsList.isEmpty
-            ? Container(
-                alignment: Alignment.center,
-                width: double.infinity,
-                padding: EdgeInsets.only(top: context.height * 0.35),
-                child: Text(
-                  context.tr('no_news_from_this_source'),
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-              )
-            : Expanded(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Expanded(
-                      child: ListView.separated(
-                        //       shrinkWrap: true,
-                        // physics: NeverScrollableScrollPhysics(),
-                        padding: EdgeInsets.all(context.width * 0.03),
-                        separatorBuilder: (context, index) =>
-                            SizedBox(height: context.width * 0.04),
-                        itemBuilder: (context, index) {
-                          return GestureDetector(
-                            onTap: () {
-                              onNewsClick(newsList[index]);
-                            },
-                            child: NewsItem(news: newsList[index]),
-                          );
-                        },
-                        itemCount: newsList.length,
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        PageNavigationWidget(
-                          visibility: newsList.isNotEmpty,
-
-                          pageNum: pageNum,
-                          onPressed: () {
-                            if (newsList.isNotEmpty) {
-                              pageNum++;
-                              setState(() {});
-                            }
-                          },
-                          text: 'next_page',
-                        ),
-                        PageNavigationWidget(
-                          visibility: pageNum > 1,
-                          pageNum: pageNum,
-                          onPressed: () {
-                            if (pageNum > 1) {
-                              pageNum--;
-                              setState(() {});
-                            }
-                          },
-                          text: 'previous_page',
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-      },
+        },
+      ),
     );
   }
 
@@ -131,4 +100,41 @@ class _NewsWidgetState extends State<NewsWidget> {
       builder: (context) => BottomSheetNewsWidget(selectedNews: selectedNews),
     );
   }
+
+  void onScroll() {
+    if (isLoading) return;
+    final double pixels = scrollController.position.pixels;
+    final double maxScroll = scrollController.position.maxScrollExtent;
+    final int triggerDistance = context.height ~/ 4;
+    if (pixels >= maxScroll - triggerDistance) {
+      loadNews();
+    }
+  }
+
+  Future<void> loadNews() async {
+    final currentSourceId = widget.source.id;
+
+    setState(() => isLoading = true);
+
+    final response = await ApiManager.getNewsBySourceId(
+      currentSourceId ?? '',
+      pageNum,
+    );
+
+    if (!mounted || currentSourceId != widget.source.id) return;
+
+    if (response.status == 'ok') {
+      setState(() {
+        newsList.addAll(response.articles!);
+        pageNum++;
+      });
+    }
+
+    setState(() => isLoading = false);
+  }
+
+  // void loadMoreItems() {
+  //   if (isLoading) return;
+  //   loadNews(); // 👈 بدل delay
+  // }
 }
